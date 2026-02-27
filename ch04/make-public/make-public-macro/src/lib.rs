8dd2fbc029770fa5d1f1dd444addc12634fdbd0e
Minggao Xi <minggao.xi@gmail.com>
2026-02-27 15:02:52 +0800

    wip

---
+++ ch04/make-public/make-public-macro/src/lib.rs
@@ -0,0 +1,29 @@
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{Data::Struct, DataStruct, DeriveInput, Fields::Named, FieldsNamed, parse_macro_input};
+
+#[proc_macro_attribute]
+pub fn public(_attr: TokenStream, item: TokenStream) -> TokenStream {
+    let ast = parse_macro_input!(item as DeriveInput);
+    let name = ast.ident;
+    let fields = match ast.data {
+        Struct(DataStruct {
+            fields: Named(FieldsNamed { ref named, .. }),
+            ..
+        }) => named,
+        _ => unimplemented!("only works for structs with named fields"),
+    };
+
+    let builder_fields = fields.iter().map(|f| {
+        let name = &f.ident;
+        let ty = &f.ty;
+        quote! { pub #name: #ty }
+    });
+
+    let public_version = quote! {
+        pub struct #name {
+            #(#builder_fields,)*
+        }
+    };
+    public_version.into()
+}
---
+++ ch04/make-public/src/main.rs
@@ -0,0 +1,11 @@
+use make_public_macro::public;
+
+#[public]
+struct Example {
+    first: String,
+    pub second: u32,
+}
+
+fn main() {
+    println!("Hello, world!");
+}
