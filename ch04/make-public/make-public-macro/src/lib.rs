b144a5ba8b08ca6fe6703cef13618333ec11c1a7
Minggao Xi <minggao.xi@gmail.com>
2026-04-07 19:56:56 +0800

    wip

--- ch04/make-public/make-public-macro/src/lib.rs
+++ ch04/make-public/make-public-macro/src/lib.rs
@@ -1,10 +1,19 @@
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{Data::Struct, DataStruct, DeriveInput, Fields::Named, FieldsNamed, parse_macro_input};
+use syn::{
+    Data::Struct,
+    DataStruct, DeriveInput,
+    Fields::Named,
+    FieldsNamed, Ident, MetaList, Token,
+    parse::{Parse, Parser},
+    parse_macro_input,
+    punctuated::Punctuated,
+};
 
 #[proc_macro_attribute]
-pub fn public(_attr: TokenStream, item: TokenStream) -> TokenStream {
+pub fn public(attr: TokenStream, item: TokenStream) -> TokenStream {
     let ast = parse_macro_input!(item as DeriveInput);
+    let excluded_fields = parse_macro_input!(attr as ExcludedFields);
     let name = ast.ident;
     let fields = match ast.data {
         Struct(DataStruct {
@@ -17,7 +26,13 @@ pub fn public(_attr: TokenStream, item: TokenStream) -> TokenStream {
     let builder_fields = fields.iter().map(|f| {
         let name = &f.ident;
         let ty = &f.ty;
-        quote! { pub #name: #ty }
+        let vis = &f.vis;
+
+        if excluded_fields.matches_ident(name) {
+            quote! { #vis #name: #ty }
+        } else {
+            quote! { pub #name: #ty }
+        }
     });
 
     let public_version = quote! {
@@ -26,4 +41,43 @@ pub fn public(_attr: TokenStream, item: TokenStream) -> TokenStream {
         }
     };
     public_version.into()
+}
+
+const EXCLUDE_ATTRIBUTE_NAME: &str = "exclude";
+
+struct ExcludedFields {
+    fields: Vec<String>,
+}
+
+impl ExcludedFields {
+    fn matches_ident(&self, name: &Option<Ident>) -> bool {
+        name.as_ref()
+            .map(|n| n.to_string())
+            .map(|n| self.fields.iter().any(|f| *f == n))
+            .unwrap_or_else(|| false)
+    }
+}
+
+impl Parse for ExcludedFields {
+    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+        match input.parse::<MetaList>() {
+            Ok(meta_list) => {
+                if meta_list
+                    .path
+                    .segments
+                    .iter()
+                    .find(|s| s.ident == EXCLUDE_ATTRIBUTE_NAME)
+                    .is_some()
+                {
+                    let parser = Punctuated::<Ident, Token![,]>::parse_terminated;
+                    let identifiers = parser.parse(meta_list.clone().tokens.into()).unwrap();
+                    let fields = identifiers.iter().map(|v| v.to_string()).collect();
+                    Ok(ExcludedFields { fields })
+                } else {
+                    Ok(ExcludedFields { fields: vec![] })
+                }
+            }
+            Err(_) => Ok(ExcludedFields { fields: vec![] }),
+        }
+    }
 }
--- ch04/make-public/src/main.rs
+++ ch04/make-public/src/main.rs
@@ -1,11 +1,26 @@
 use make_public_macro::public;
 
-#[public]
+#[public(exclude(third))]
 struct Example {
     first: String,
     pub second: u32,
+    third: bool,
 }
 
+impl Example {
+    pub fn new() -> Self {
+        Self {
+            first: "first".to_string(),
+            second: 5,
+            third: true,
+        }
+    }
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let e = Example::new();
+    println!("{}", e.first);
+    println!("{}", e.second);
+    println!("{}", e.third);
 }
