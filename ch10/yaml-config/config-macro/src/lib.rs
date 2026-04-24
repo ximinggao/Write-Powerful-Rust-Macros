4f88531fd6722b16af5c0a2281ef6c21e5db5a22
Minggao Xi <minggao.xi@gmail.com>
2026-04-24 17:13:35 +0800

    Add docs

--- ch10/yaml-config/config-macro/src/lib.rs
+++ ch10/yaml-config/config-macro/src/lib.rs
@@ -1,3 +1,8 @@
+#![doc = include_str!("../README.md")]
+
+//! ## Documentation from lib.rs
+//! Here is documentation placed directly within lib.rs...
+
 use std::{collections::HashMap, fs};
 
 use proc_macro::TokenStream;
@@ -14,6 +19,9 @@ mod output;
 #[cfg(feature = "struct")]
 mod struct_output;
 
+/// This function-like macro will generate a struct called `Config`
+/// which contains a 'HashMap<String, String>' with all the yaml
+/// config properties.
 #[cfg(feature = "functional")]
 #[proc_macro]
 pub fn config(item: TokenStream) -> TokenStream {
@@ -40,7 +48,23 @@ fn find_yaml_values(input: &ConfigInput) -> Result<HashMap<String, String>, syn:
     serde_yaml::from_reader(file).map_err(|e| syn::Error::new(Span::call_site(), e.to_string()))
 }
 
-#[cfg(feature = "struct")]
+/// This macro allows manipulation of an existing struct to serve as a 'config' struct.
+/// It will replace any existing fields with those present in the configuration.
+/// 
+/// ```rust
+/// use config_macro::config_struct;
+/// 
+/// #[config_struct(path = "./configuration/config.yaml")]
+/// struct Example {}
+/// 
+/// // Example now has a new method
+/// let e = Example::new();
+/// 
+/// // e now contains a 'user' field that we can access
+/// println!("{}", e.user);
+/// ```
+/// 
+#[cfg(any(feature = "struct", doc))]
 #[proc_macro_attribute]
 pub fn config_struct(attr: TokenStream, item: TokenStream) -> TokenStream {
     let input: ConfigInput = parse_macro_input!(attr);
